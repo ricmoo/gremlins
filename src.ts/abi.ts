c9faba67c8902b5631ea37b0519051f3b62e4aa8
Richard Moore <me@ricmoo.com>
2023-09-23 04:56:35 -0400

    Added stubs for Gremlins ABI format.

---
+++ src.ts/abi.ts
@@ -0,0 +1,8 @@
+
+export function decode<T = any>(data: string): T {
+    throw new Error("not implemented");
+}
+
+export function encode(data: any): string {
+    throw new Error("not implemented");
+}
